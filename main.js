ac0dabc05bc4d49826cc4bcc2949c759d1baa05c
milenaggoes <rm94305@fiap.com.br>
2023-01-10 19:21:05 -0300

    Projeto Finalizado

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
 }
 
-//Referências
-const listaDeTeclas = document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
-while (contador < listaDeTeclas.length) {
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
 
-    listaDeTeclas[contador].onclick = tocaSom;
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
 
-    contador = contador + 1;
+    tecla.onkeydown = function (evento) {
 
-    console.log(contador);
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
